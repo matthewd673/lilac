465948d28cd2203dd4286787ec8e46a207bb122e
Matthew Daly <matthewd673@outlook.com>
2024-02-27 15:39:21 -0500

    Add analyses command

--- lib/lilac.rb
+++ lib/lilac.rb
@@ -1,13 +1,26 @@
 # typed: strict
 require_relative "ansi"
+require_relative "analysis/analyses"
 
 extend T::Sig
 
 sig { void }
 def main
   puts(ANSI.fmt256("lilac", ANSI::LILAC_256, bold: true))
+  puts("  analyses: list all analyses")
+end
+
+sig { void }
+def print_analyses
+  for a in ANALYSES
+    puts(a.id)
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
-  main
+  if ARGV.length > 0 and ARGV[0] == "analyses"
+    print_analyses
+  else
+    main
+  end
 end
