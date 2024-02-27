627c14074abc305c903d364e0e83ee3b9b8aa0fd
Matthew Daly <matthewd673@outlook.com>
2024-02-27 12:34:48 -0500

    Add ANSI.fmt256, remove testing from lilac.rb

--- lib/ansi.rb
+++ lib/ansi.rb
@@ -24,14 +24,26 @@ module ANSI
   CYAN_BRIGHT = 96
   WHITE_BRIGHT = 97
 
+  LILAC_256 = 147
+
   sig { params(obj: Object, color: Integer, bold: T::Boolean).returns(String) }
   # Format a String using ANSI codes.
   #
-  # @param [Integer] color The foreground color of the String.
+  # @param [Integer] color The foreground color of the String. Should be
+  #   one of the constants provided in the module.
   # @param [T::Boolean] bold Determines if the String is bold or not.
   #
   # @return [String] The formatted String which can be printed as normal.
   def self.fmt(obj, color: ANSI::DEFAULT, bold: false)
     "\e[#{color}m#{"\e[1m" unless not bold}#{obj.to_s}\e[0m"
+  end
+
+  sig { params(obj: Object, color: Integer, bold: T::Boolean).returns(String) }
+  # Format a String using 256-color.
+  #
+  # @param [Integer] color The number of the color in the xterm-256 color table.
+  # @param [T::Boolean] bold Determines if the String is bold or not.
+  def self.fmt256(obj, color, bold: false)
+    "\e[38;5;#{color}m#{"\e[1m" unless not bold}#{obj.to_s}\e[0m"
   end
 end
--- lib/lilac.rb
+++ lib/lilac.rb
@@ -1,45 +1,13 @@
-# typed: true
-require_relative "il"
-require_relative "interpreter"
-require_relative "debugger/pretty_printer"
-require_relative "optimization/runner"
-require_relative "optimization/lvn"
-require_relative "optimization/bb"
+# typed: strict
+require_relative "ansi"
 
-include IL
+extend T::Sig
+
+sig { void }
+def main
+  puts(ANSI.fmt256("lilac", ANSI::LILAC_256, bold: true))
+end
 
 if $PROGRAM_NAME == __FILE__
-  program = Program.new
-  program.push_stmt(Declaration.new(Type::I32,
-                                    ID.new("a"),
-                                    BinaryOp.new(BinaryOp::ADD_OP,
-                                                 Constant.new(Type::I32,
-                                                              2),
-                                                 Constant.new(Type::I32,
-                                                              3))))
-  program.push_stmt(Declaration.new(Type::I32,
-                                    ID.new("b"),
-                                    Constant.new(Type::I32,
-                                                 10)))
-
-  program.push_stmt(Declaration.new(Type::I32,
-                                    ID.new("c"),
-                                    BinaryOp.new(BinaryOp::MUL_OP,
-                                                 ID.new("a"),
-                                                 ID.new("b"))))
-
-  program.push_stmt(Assignment.new(ID.new("c"),
-                                   BinaryOp.new(BinaryOp::SUB_OP,
-                                                ID.new("c"),
-                                                ID.new("b"))))
-
-  Interpreter.interpret(program)
-
-  blocks = BB.create_blocks(program)
-  pretty_printer = Debugger::PrettyPrinter.new
-  pretty_printer.print_blocks(blocks)
-
-  # TODO: temp (though everything here is temp)
-  runner = Runner.new(program)
-  runner.run_pass(CondenseLabels.new)
+  main
 end
