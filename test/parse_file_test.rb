3f8ad3300787ad1a0e92acd327671769476d389b
Matthew Daly <matthewd673@outlook.com>
2024-04-09 13:32:24 -0400

    Add more frontend tests

--- test/parse_file_test.rb
+++ test/parse_file_test.rb
@@ -33,4 +33,87 @@ class ParseFileTest < Minitest::Test
 
     assert program.eql?(expected)
   end
+
+  sig { void }
+  def test_parse_label_and_jmp
+    expected = Program.new(stmt_list: [
+      Label.new("L0"),
+      Definition.new(Type::I32, ID.new("a"), Constant.new(Type::I32, 3)),
+      Jump.new("L0"),
+    ])
+    program = Frontend::Parser::parse_file("test/il_programs/label_and_jmp.txt")
+
+    assert program.eql?(expected)
+  end
+
+  sig { void }
+  def test_parse_jz_jnz
+    expected = Program.new(stmt_list: [
+      Label.new("L0"),
+      Definition.new(Type::I32, ID.new("a"), Constant.new(Type::I32, 5)),
+      JumpNotZero.new(ID.new("a"), "L0"),
+      JumpZero.new(Constant.new(Type::U8, 1), "L0"),
+    ])
+    program = Frontend::Parser::parse_file("test/il_programs/jz_jnz.txt")
+
+    assert program.eql?(expected)
+  end
+
+  sig { void }
+  def test_parse_binop
+    expected = Program.new(stmt_list: [
+      Definition.new(Type::I32, ID.new("a", number: 0), BinaryOp.new(
+        BinaryOp::Operator::ADD,
+        Constant.new(Type::I32, 12), Constant.new(Type::I32, 6)
+      )),
+      Definition.new(Type::I32, ID.new("a", number: 1), BinaryOp.new(
+        BinaryOp::Operator::SUB,
+        ID.new("a", number: 0), Constant.new(Type::I32, 0)
+      )),
+      Definition.new(Type::I32, ID.new("a", number: 2), BinaryOp.new(
+        BinaryOp::Operator::MUL,
+        ID.new("a", number: 1), Constant.new(Type::I32, -2)
+      )),
+      Definition.new(Type::I32, ID.new("a", number: 3), BinaryOp.new(
+        BinaryOp::Operator::DIV,
+        ID.new("a", number: 2), Constant.new(Type::I32, -2)
+      )),
+
+      Definition.new(Type::U8, ID.new("b"), BinaryOp.new(
+        BinaryOp::Operator::EQ,
+        Constant.new(Type::U8, 0), Constant.new(Type::U8, 0)
+      )),
+      Definition.new(Type::U8, ID.new("c"), BinaryOp.new(
+        BinaryOp::Operator::NEQ,
+        Constant.new(Type::U8, 1), Constant.new(Type::U8, 0)
+      )),
+      Definition.new(Type::U8, ID.new("d"), BinaryOp.new(
+        BinaryOp::Operator::LT,
+        Constant.new(Type::U8, 2), Constant.new(Type::U8, 4)
+      )),
+      Definition.new(Type::U8, ID.new("e"), BinaryOp.new(
+        BinaryOp::Operator::GT,
+        Constant.new(Type::U8, 3), Constant.new(Type::U8, 1)
+      )),
+      Definition.new(Type::U8, ID.new("f"), BinaryOp.new(
+        BinaryOp::Operator::LEQ,
+        Constant.new(Type::U8, 1), Constant.new(Type::U8, 1)
+      )),
+      Definition.new(Type::U8, ID.new("g"), BinaryOp.new(
+        BinaryOp::Operator::GEQ,
+        Constant.new(Type::U8, 0), Constant.new(Type::U8, 1)
+      )),
+      Definition.new(Type::U8, ID.new("h"), BinaryOp.new(
+        BinaryOp::Operator::OR,
+        Constant.new(Type::U8, 1), Constant.new(Type::U8, 0)
+      )),
+      Definition.new(Type::U8, ID.new("i"), BinaryOp.new(
+        BinaryOp::Operator::AND,
+        Constant.new(Type::U8, 0), Constant.new(Type::U8, 1)
+      )),
+    ])
+    program = Frontend::Parser::parse_file("test/il_programs/binop.txt")
+
+    assert program.eql?(expected)
+  end
 end
