556e38846f3987c641edec3d227614323f48a457
Matthew Daly <matthewd673@outlook.com>
2024-04-08 21:18:17 -0400

    Add test_parse_types

--- test/parse_file_test.rb
+++ test/parse_file_test.rb
@@ -11,20 +11,26 @@ class ParseFileTest < Minitest::Test
 
   sig { void }
   def test_parse_definition
-    expected = Program.new
-    expected.stmt_list.push(Definition.new(Type::I32,
-                                           ID.new("a"),
-                                           Constant.new(Type::I32, 5)))
-
+    expected = Program.new(stmt_list: [
+      Definition.new(Type::I32, ID.new("a"), Constant.new(Type::I32, 5)),
+    ])
     program = Frontend::Parser::parse_file("test/il_programs/definition.txt")
 
     assert program.eql?(expected)
   end
 
-  private
+  sig { void }
+  def test_parse_types
+    expected = Program.new(stmt_list: [
+      Definition.new(Type::U8, ID.new("a"), Constant.new(Type::U8, 8)),
+      Definition.new(Type::I16, ID.new("b"), Constant.new(Type::I16, -3)),
+      Definition.new(Type::I32, ID.new("c"), Constant.new(Type::I32, 5)),
+      Definition.new(Type::I64, ID.new("d"), Constant.new(Type::I64, 9999)),
+      Definition.new(Type::F32, ID.new("e"), Constant.new(Type::F32, 3.14)),
+      Definition.new(Type::F64, ID.new("f"), Constant.new(Type::F64, -1.0)),
+    ])
+    program = Frontend::Parser::parse_file("test/il_programs/types.txt")
 
-  sig { params(a: Program, b: Program).returns(T::Boolean) }
-  def program_match?(a, b)
-    true # TODO
+    assert program.eql?(expected)
   end
 end
