0edfb2ebca53a77fb80e745d7ba0b5fcf2b0f9c6
Matthew Daly <matthewd673@outlook.com>
2024-04-29 18:26:50 -0400

    Add WasmTranslator to translate IL
    WatGenerator's only responsible is to stringify. CodeGen::Generator API has been updated to reflect this.

--- lib/code_gen/generator.rb
+++ lib/code_gen/generator.rb
@@ -1,16 +1,15 @@
 # typed: strict
 require "sorbet-runtime"
 require_relative "code_gen"
-require_relative "../il"
+require_relative "instruction"
 
 class CodeGen::Generator
   extend T::Sig
 
   include CodeGen
 
-  sig { params(transformer: ILTransformer, cfg_program: IL::CFGProgram).void }
-  def initialize(transformer, cfg_program)
-    @transformer = transformer
-    @program = cfg_program
+  sig { params(root_component: Component).void }
+  def initialize(root_component)
+    @root_component = root_component
   end
 end
