c876cf043169bbcd458d54cb1dbb39243cfbfa6f
Matthew Daly <matthewd673@outlook.com>
2024-05-03 15:23:02 -0400

    Create Transformations module and move ToSSA to it

---
+++ lib/transformations/transformations.rb
@@ -0,0 +1,9 @@
+# typed: strict
+# frozen_string_literal: true
+
+require "sorbet"
+
+# The Transformations module contains transformations for IL Programs.
+module Transformations
+  # NOTE: namespace stub
+end
