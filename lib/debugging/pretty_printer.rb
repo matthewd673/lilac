1d92df6f445dca870ebe49cb22fd391dcbbf591d
Matthew Daly <matthewd673@outlook.com>
2024-05-09 17:11:38 -0400

    Reorganize PrettyPrinter

--- lib/debugging/pretty_printer.rb
+++ lib/debugging/pretty_printer.rb
@@ -73,7 +73,8 @@ module Debugging
     # Pretty-print a collection of basic blocks (+Analysis::BB::Block+)
     # to the terminal with headers for each block and syntax highlighting.
     #
-    # @param [T::Array[Analysis::BB::Block]] blocks The array of blocks to print.
+    # @param [T::Array[Analysis::BB::Block]] blocks The array of blocks to
+    #   print.
     def print_blocks(blocks)
       blocks.each do |b|
         puts(ANSI.fmt("[BLOCK (len=#{b.length})]", bold: true))
@@ -83,263 +84,219 @@ module Debugging
       end
     end
 
-    protected
+    private
 
     # A visit context used internally by the PrettyPrinter Visitor.
     PrettyPrinterContext = Struct.new(:gutter_len, :line_num, :indent)
 
-    VISIT_TYPE = T.let(lambda { |v, o, c|
-      ANSI.fmt(o.to_s, color: PALETTE[IL::Type])
-    }, Visitor::Lambda)
-
-    VISIT_VALUE = T.let(lambda { |v, o, c|
-      o.to_s # NOTE: stub, don't format
-    }, Visitor::Lambda)
-
-    VISIT_CONSTANT = T.let(lambda { |v, o, c|
-      if o.type.eql?(IL::Type::Void)
-        ANSI.fmt("void", color: PALETTE[IL::Constant])
-      else
-        ANSI.fmt(o.to_s, color: PALETTE[IL::Constant])
-      end
-    }, Visitor::Lambda)
-
-    VISIT_ID = T.let(lambda { |v, o, c|
-      "#{ANSI.fmt(o.name, color: PALETTE[IL::ID])}##{o.number}"
-    }, Visitor::Lambda)
-
-    VISIT_REGISTER = T.let(lambda { |v, o, c|
-      ANSI.fmt(o.name, color: PALETTE[IL::Register]).to_s
-    }, Visitor::Lambda)
-
-    VISIT_EXPRESSION = T.let(lambda { |v, o, c|
-      o.to_s # NOTE: stub, don't format
-    }, Visitor::Lambda)
-
-    VISIT_BINARYOP = T.let(lambda { |v, o, c|
-      s = "#{v.visit(o.left)} "
-      s += "#{ANSI.fmt(o.op, color: PALETTE[IL::BinaryOp])} "
-      s += v.visit(o.right)
-      s
-    }, Visitor::Lambda)
-
-    VISIT_CALL = T.let(lambda { |v, o, c|
-      s = "#{ANSI.fmt("call", bold: true)} #{o.func_name}("
-      o.args.each do |a|
-        s += "#{v.visit(a)}, "
-      end
-      s.chomp!(", ")
-      s += ")"
-    }, Visitor::Lambda)
-
-    VISIT_EXTERNCALL = T.let(lambda  { |v, o, c|
-      s = "#{ANSI.fmt("extern call", bold: true)} #{o.func_source} #{o.func_name}("
-      o.args.each do |a|
-        s += "#{v.visit(a)}, "
-      end
-      s.chomp!(", ")
-      s += ")"
-    }, Visitor::Lambda)
-
-    VISIT_STATEMENT = T.let(lambda { |v, o, c|
-      o.to_s # NOTE: stub, don't format
-    }, Visitor::Lambda)
-
-    VISIT_DEFINITION = T.let(lambda { |v, o, c|
-      # line number and indent
-      num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
-      pad = indent(c.indent)
-      s = ANSI.fmt(num, color: PALETTE[:gutter]) + pad
-
-      s += "#{v.visit(o.type)} #{v.visit(o.id)} = #{v.visit(o.rhs)}"
-
-      # annotation
-      if o.annotation
-        s += ANSI.fmt(" \" #{o.annotation}", color: PALETTE[:annotation])
-      end
-
-      s
-    }, Visitor::Lambda)
-
-    VISIT_LABEL = T.let(lambda { |v, o, c|
-      # line number and indent
-      num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
-      pad = indent(c.indent)
-      s = ANSI.fmt(num, color: PALETTE[:gutter]) + pad
-
-      s += ANSI.fmt("#{o.name}:", color: PALETTE[IL::Label])
-
-      # annotation
-      if o.annotation
-        s += ANSI.fmt(" \" #{o.annotation}", color: PALETTE[:annotation])
-      end
-
-      s
-    }, Visitor::Lambda)
-
-    VISIT_JUMP = T.let(lambda { |v, o, c|
-      # line number and indent
-      num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
-      pad = indent(c.indent)
-      s = ANSI.fmt(num, color: PALETTE[:gutter]) + pad
-
-      s += "#{ANSI.fmt("jmp", color: PALETTE[IL::Jump])} "
-      s += ANSI.fmt(o.target, color: PALETTE[IL::Label])
-
-      # annotation
-      if o.annotation
-        s += ANSI.fmt(" \" #{o.annotation}", color: PALETTE[:annotation])
-      end
-
-      s
-    }, Visitor::Lambda)
-
-    VISIT_JUMPZERO = T.let(lambda { |v, o, c|
-      # line number and indent
-      num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
-      pad = indent(c.indent)
-      s = ANSI.fmt(num, color: PALETTE[:gutter]) + pad
-
-      s += "#{ANSI.fmt("jz", color: PALETTE[IL::Jump])} "
-      s += "#{v.visit(o.cond)} #{ANSI.fmt(o.target, color: PALETTE[IL::Label])}"
-
-      # annotation
-      if o.annotation
-        s += ANSI.fmt(" \" #{o.annotation}", color: PALETTE[:annotation])
-      end
-
-      s
-    }, Visitor::Lambda)
-
-    VISIT_JUMPNOTZERO = T.let(lambda { |v, o, c|
-      # line number and indent
-      num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
-      pad = indent(c.indent)
-      s = ANSI.fmt(num, color: PALETTE[:gutter]) + pad
-
-      s += "#{ANSI.fmt("jnz", color: PALETTE[IL::Jump])} "
-      s += "#{v.visit(o.cond)} #{ANSI.fmt(o.target, color: PALETTE[IL::Label])}"
-
-      # annotation
-      if o.annotation
-        s += ANSI.fmt(" \" #{o.annotation}", color: PALETTE[:annotation])
-      end
-
-      s
-    }, Visitor::Lambda)
-
-    VISIT_RETURN = T.let(lambda { |v, o, c|
-      # line number and indent
-      num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
-      pad = indent(c.indent)
-      s = ANSI.fmt(num, color: PALETTE[:gutter]) + pad
-
-      s += "#{ANSI.fmt("ret", bold: true)} #{v.visit(o.value)}"
-
-      # annotation
-      if o.annotation
-        s += ANSI.fmt(" \" #{o.annotation}", color: PALETTE[:annotation])
-      end
-
-      s
-    }, Visitor::Lambda)
-
-    VISIT_VOIDCALL = T.let(lambda { |v, o, c|
-      # line number and indent
-      num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
-      pad = indent(c.indent)
-      s = ANSI.fmt(num, color: PALETTE[:gutter]) + pad
-
-      s += "#{ANSI.fmt("void", bold: true)} #{v.visit(o.call)}"
-
-      # annotation
-      if o.annotation
-        s += ANSI.fmt(" \" #{o.annotation}", color: PALETTE[:annotation])
-      end
-
-      s
-    }, Visitor::Lambda)
-
-    VISIT_FUNCDEF = T.let(lambda { |v, o, c|
-      # line number
-      num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
-      c.line_num += 1
-      s = ANSI.fmt(num, color: PALETTE[:gutter])
-
-      s += "#{ANSI.fmt("func", bold: true)} #{o.name}("
-
-      # print params
-      o.params.each do |p|
-        s += "#{v.visit(p.type)} #{v.visit(p.id)}, "
-      end
-      s.chomp!(", ")
-
-      s += ") -> #{v.visit(o.ret_type)}\n"
-
-      # print body
-      c.indent = 1
-      o.stmt_list.each do |stmt|
-        s += "#{v.visit(stmt, ctx: c)}\n"
-        c.line_num += 1
-      end
-
-      # fix line number and indent
-      c.line_num -= 1
-      c.indent = 0
-
-      # print end
-      new_num = left_pad(c.line_num.to_s, c.gutter_len)
-      s += "#{ANSI.fmt(new_num, color: PALETTE[:gutter])} "
-      s += ANSI.fmt("end", bold: true).to_s
-
-      s
-    }, Visitor::Lambda)
-
-    VISIT_FUNCPARAM = T.let(lambda { |v, o, c|
-      "#{v.visit(o.type)} #{v.visit(o.id)}"
-    }, Visitor::Lambda)
-
-    VISIT_EXTERNFUNCDEF = T.let(lambda { |v, o, c|
-      # line number
-      num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
-      c.line_num += 1
-      s = ANSI.fmt(num, color: PALETTE[:gutter])
-
-      s += "#{ANSI.fmt("extern func", bold: true)} #{o.source} #{o.name}("
-
-      # print param types
-      o.param_types.each do |t|
-        s += "#{v.visit(t)}, "
-      end
-      s.chomp!(", ")
-
-      s += ") -> #{v.visit(o.ret_type)}\n"
-
-      # fix line number and indent
-      c.line_num -= 1
-      c.indent = 0
-      s
-    }, Visitor::Lambda)
-
     VISIT_LAMBDAS = T.let({
-      IL::Type => VISIT_TYPE,
-      IL::Constant => VISIT_CONSTANT,
-      IL::ID => VISIT_ID,
-      IL::Register => VISIT_REGISTER,
-      IL::BinaryOp => VISIT_BINARYOP,
-      IL::Call => VISIT_CALL,
-      IL::ExternCall => VISIT_EXTERNCALL,
-      IL::Statement => VISIT_STATEMENT,
-      IL::Definition => VISIT_DEFINITION,
-      IL::Label => VISIT_LABEL,
-      IL::Jump => VISIT_JUMP,
-      IL::JumpZero => VISIT_JUMPZERO,
-      IL::JumpNotZero => VISIT_JUMPNOTZERO,
-      IL::Return => VISIT_RETURN,
-      IL::VoidCall => VISIT_VOIDCALL,
-      IL::FuncDef => VISIT_FUNCDEF,
-      IL::FuncParam => VISIT_FUNCPARAM,
-      IL::ExternFuncDef => VISIT_EXTERNFUNCDEF,
+      IL::Type => lambda { |v, o, c|
+        ANSI.fmt(o.to_s, color: PALETTE[IL::Type])
+      },
+      IL::Constant => lambda { |v, o, c|
+        if o.type.eql?(IL::Type::Void)
+          ANSI.fmt("void", color: PALETTE[IL::Constant])
+        else
+          ANSI.fmt(o.to_s, color: PALETTE[IL::Constant])
+        end
+      },
+      IL::ID => lambda { |v, o, c|
+        "#{ANSI.fmt(o.name, color: PALETTE[IL::ID])}##{o.number}"
+      },
+      IL::Register => lambda { |v, o, c|
+        ANSI.fmt(o.name, color: PALETTE[IL::Register]).to_s
+      },
+      IL::BinaryOp => lambda { |v, o, c|
+        s = "#{v.visit(o.left)} "
+        s += "#{ANSI.fmt(o.op, color: PALETTE[IL::BinaryOp])} "
+        s += v.visit(o.right)
+        s
+      },
+      IL::Call => lambda { |v, o, c|
+        s = "#{ANSI.fmt("call", bold: true)} #{o.func_name}("
+        o.args.each do |a|
+          s += "#{v.visit(a)}, "
+        end
+        s.chomp!(", ")
+        s += ")"
+      },
+      IL::ExternCall => lambda  { |v, o, c|
+        s = "#{ANSI.fmt("extern call", bold: true)} "\
+            "#{o.func_source} #{o.func_name}("
+        o.args.each do |a|
+          s += "#{v.visit(a)}, "
+        end
+        s.chomp!(", ")
+        s += ")"
+      },
+      IL::Definition => lambda { |v, o, c|
+        # line number and indent
+        num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
+        pad = indent(c.indent)
+        s = ANSI.fmt(num, color: PALETTE[:gutter]) + pad
+
+        s += "#{v.visit(o.type)} #{v.visit(o.id)} = #{v.visit(o.rhs)}"
+
+        # annotation
+        if o.annotation
+          s += ANSI.fmt(" \" #{o.annotation}", color: PALETTE[:annotation])
+        end
+
+        s
+      },
+      IL::Label => lambda { |v, o, c|
+        # line number and indent
+        num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
+        pad = indent(c.indent)
+        s = ANSI.fmt(num, color: PALETTE[:gutter]) + pad
+
+        s += ANSI.fmt("#{o.name}:", color: PALETTE[IL::Label])
+
+        # annotation
+        if o.annotation
+          s += ANSI.fmt(" \" #{o.annotation}", color: PALETTE[:annotation])
+        end
+
+        s
+      },
+      IL::Jump => lambda { |v, o, c|
+        # line number and indent
+        num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
+        pad = indent(c.indent)
+        s = ANSI.fmt(num, color: PALETTE[:gutter]) + pad
+
+        s += "#{ANSI.fmt("jmp", color: PALETTE[IL::Jump])} "
+        s += ANSI.fmt(o.target, color: PALETTE[IL::Label])
+
+        # annotation
+        if o.annotation
+          s += ANSI.fmt(" \" #{o.annotation}", color: PALETTE[:annotation])
+        end
+
+        s
+      },
+      IL::JumpZero => lambda { |v, o, c|
+        # line number and indent
+        num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
+        pad = indent(c.indent)
+        s = ANSI.fmt(num, color: PALETTE[:gutter]) + pad
+
+        s += "#{ANSI.fmt("jz", color: PALETTE[IL::Jump])} "\
+             "#{v.visit(o.cond)} "\
+             "#{ANSI.fmt(o.target, color: PALETTE[IL::Label])}"
+
+        # annotation
+        if o.annotation
+          s += ANSI.fmt(" \" #{o.annotation}", color: PALETTE[:annotation])
+        end
+
+        s
+      },
+      IL::JumpNotZero => lambda { |v, o, c|
+        # line number and indent
+        num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
+        pad = indent(c.indent)
+        s = ANSI.fmt(num, color: PALETTE[:gutter]) + pad
+
+        s += "#{ANSI.fmt("jnz", color: PALETTE[IL::Jump])} "\
+             "#{v.visit(o.cond)} "\
+             "#{ANSI.fmt(o.target, color: PALETTE[IL::Label])}"
+
+        # annotation
+        if o.annotation
+          s += ANSI.fmt(" \" #{o.annotation}", color: PALETTE[:annotation])
+        end
+
+        s
+      },
+      IL::Return => lambda { |v, o, c|
+        # line number and indent
+        num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
+        pad = indent(c.indent)
+        s = ANSI.fmt(num, color: PALETTE[:gutter]) + pad
+
+        s += "#{ANSI.fmt("ret", bold: true)} #{v.visit(o.value)}"
+
+        # annotation
+        if o.annotation
+          s += ANSI.fmt(" \" #{o.annotation}", color: PALETTE[:annotation])
+        end
+
+        s
+      },
+      IL::VoidCall => lambda { |v, o, c|
+        # line number and indent
+        num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
+        pad = indent(c.indent)
+        s = ANSI.fmt(num, color: PALETTE[:gutter]) + pad
+
+        s += "#{ANSI.fmt("void", bold: true)} #{v.visit(o.call)}"
+
+        # annotation
+        if o.annotation
+          s += ANSI.fmt(" \" #{o.annotation}", color: PALETTE[:annotation])
+        end
+
+        s
+      },
+      IL::FuncDef => lambda { |v, o, c|
+        # line number
+        num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
+        c.line_num += 1
+        s = ANSI.fmt(num, color: PALETTE[:gutter])
+
+        s += "#{ANSI.fmt("func", bold: true)} #{o.name}("
+
+        # print params
+        o.params.each do |p|
+          s += "#{v.visit(p.type)} #{v.visit(p.id)}, "
+        end
+        s.chomp!(", ")
+
+        s += ") -> #{v.visit(o.ret_type)}\n"
+
+        # print body
+        c.indent = 1
+        o.stmt_list.each do |stmt|
+          s += "#{v.visit(stmt, ctx: c)}\n"
+          c.line_num += 1
+        end
+
+        # fix line number and indent
+        c.line_num -= 1
+        c.indent = 0
+
+        # print end
+        new_num = left_pad(c.line_num.to_s, c.gutter_len)
+        s += "#{ANSI.fmt(new_num, color: PALETTE[:gutter])} "
+        s += ANSI.fmt("end", bold: true).to_s
+
+        s
+      },
+      IL::FuncParam => lambda { |v, o, c|
+        "#{v.visit(o.type)} #{v.visit(o.id)}"
+      },
+      IL::ExternFuncDef => lambda { |v, o, c|
+        # line number
+        num = "#{left_pad(c.line_num.to_s, c.gutter_len)} "
+        c.line_num += 1
+        s = ANSI.fmt(num, color: PALETTE[:gutter])
+
+        s += "#{ANSI.fmt("extern func", bold: true)} #{o.source} #{o.name}("
+
+        # print param types
+        o.param_types.each do |t|
+          s += "#{v.visit(t)}, "
+        end
+        s.chomp!(", ")
+
+        s += ") -> #{v.visit(o.ret_type)}\n"
+
+        # fix line number and indent
+        c.line_num -= 1
+        c.indent = 0
+        s
+      },
     }.freeze, Visitor::LambdaHash)
 
     private
