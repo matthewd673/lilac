5b8994d108579dd8f40fe5a61a6bb0356f5f446e
Matthew Daly <matthewd673@outlook.com>
2024-05-22 11:51:24 -0400

    Remove 3 label and jump optimizations
    These optimizations should be performed by operating on a CFG, not scanning through the IL

--- lib/optimization/optimizations.rb
+++ lib/optimization/optimizations.rb
@@ -3,22 +3,16 @@
 
 require "sorbet-runtime"
 require_relative "optimization_pass"
-require_relative "condense_labels"
 require_relative "lvn"
 require_relative "const_cond_jumps"
-require_relative "remove_unused_labels"
-require_relative "remove_useless_jumps"
 require_relative "simplify_redundant_binops"
 
 module Lilac
   module Optimization
     # A definitive list of all optimizations available in Lilac.
     OPTIMIZATIONS = T.let([
-      CondenseLabels,
       LVN,
       ConstCondJumps,
-      RemoveUnusedLabels,
-      RemoveUselessJumps,
       SimplifyRedundantBinops,
     ].freeze, T::Array[T.class_of(OptimizationPass)])
   end
