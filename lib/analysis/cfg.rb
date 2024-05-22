085860c511678b6df2be311470e2b4544f79ce8c
Matthew Daly <matthewd673@outlook.com>
2024-05-22 13:12:49 -0400

    Replace CFG constructor with .from_blocks
    Calling CFG.new will now just create a CFG with an ENTRY and EXIT

--- lib/analysis/cfg.rb
+++ lib/analysis/cfg.rb
@@ -33,19 +33,23 @@ module Lilac
       # @return [BB::Block] The EXIT block object in this CFG.
       attr_reader :exit
 
-      sig { params(block_list: T::Array[BB]).void }
-      # Construct a new CFG from a list of basic blocks.
-      #
-      # @param [T::Array[BB]] block_list The list of basic blocks which will be
-      #   the nodes of this CFG.
-      def initialize(block_list)
+      sig { void }
+      def initialize
         super()
 
-        @label_map = T.let({}, T::Hash[String, BB])
+        # create an entry and an exit with an edge connecting them
         @entry = T.let(BB.new(ENTRY, stmt_list: []), BB)
         @exit = T.let(BB.new(EXIT, stmt_list: []), BB)
+        add_edge(Edge.new(@entry, @exit))
+      end
 
-        compute_graph(block_list)
+      sig { params(blocks: T::Array[BB]).returns(CFG) }
+      def self.from_blocks(blocks)
+        cfg = CFG.new
+
+        compute_graph(cfg, blocks)
+
+        cfg
       end
 
       sig { params(block: T.proc.params(arg0: BB).void).void }
@@ -60,24 +64,62 @@ module Lilac
       # @param [BB] node The basic block to add.
       def add_node(node)
         @nodes.push(node)
-        return unless node.entry&.is_a?(IL::Label)
+      end
 
-        @label_map[T.unsafe(node.entry).name] = node
+      protected
+
+      sig { returns(T::Array[BB]) }
+      def nodes
+        @nodes
+      end
+
+      sig { returns(T::Array[Graph::Edge[BB]]) }
+      def edges
+        @edges
+      end
+
+      sig { returns(T::Hash[BB, T::Set[Graph::Edge[BB]]]) }
+      def incoming
+        @incoming
+      end
+
+      sig { returns(T::Hash[BB, T::Set[Graph::Edge[BB]]]) }
+      def outgoing
+        @outgoing
+      end
+
+      sig { params(bb: BB).void }
+      def entry=(bb)
+        @entry = bb
+      end
+
+      sig { params(bb: BB).void }
+      def exit=(bb)
+        @exit = bb
       end
 
       private
 
-      sig { params(block_list: T::Array[BB]).void }
-      def compute_graph(block_list)
+      sig { params(cfg: CFG, block_list: T::Array[BB]).void }
+      def self.compute_graph(cfg, block_list)
         # just in case this gets run more than once
-        @nodes.clear
-        @edges.clear
-        @incoming.clear
-        @outgoing.clear
+        cfg.nodes.clear
+        cfg.edges.clear
+        cfg.incoming.clear
+        cfg.outgoing.clear
+
+        cfg.entry = T.let(BB.new(ENTRY, stmt_list: []), BB)
+        cfg.exit = T.let(BB.new(EXIT, stmt_list: []), BB)
+
+        label_map = {}
 
         # add all blocks to the graph nodes
         block_list.each do |b|
-          add_node(b)
+          cfg.add_node(b)
+
+          if b.entry
+            label_map[T.unsafe(b.entry).name] = b
+          end
         end
 
         # connect blocks into graph nodes
@@ -87,7 +129,7 @@ module Lilac
             jump = T.unsafe(b.exit) # to placate Sorbet below
 
             # find block that jump is targeting
-            successor = @label_map[jump.target]
+            successor = label_map[jump.target]
             unless successor # this is unlikely but I think possible
               raise "CFG attempted to build edge to label that doesn't exist: "\
                     "\"#{jump.target}\""
@@ -97,7 +139,7 @@ module Lilac
             # if jump IS conditional then the edge must be (and we can easily
             # check if a jump is conditional based on its class)
             successor.true_branch = jump.class != IL::Jump
-            add_edge(Edge.new(b, successor))
+            cfg.add_edge(Edge.new(b, successor))
 
             # if jump is NOT conditional then stop after creating this edge
             if jump.instance_of?(IL::Jump)
@@ -109,22 +151,22 @@ module Lilac
           following = block_list[i + 1]
           if following
             following.true_branch = false
-            add_edge(Edge.new(b, following))
+            cfg.add_edge(Edge.new(b, following))
           else # reached the end, point to exit
-            @exit.true_branch = false
-            add_edge(Edge.new(b, @exit))
+            cfg.exit.true_branch = false
+            cfg.add_edge(Edge.new(b, cfg.exit))
           end
         end
 
         # create edge from entry to first block
-        first_block = @nodes[0]
-        first_block ||= @exit
+        first_block = cfg.nodes[0]
+        first_block ||= cfg.exit
         first_block.true_branch = false
-        add_edge(Edge.new(entry, first_block))
+        cfg.add_edge(Edge.new(cfg.entry, first_block))
 
         # add entry and exit block nodes to graph
-        @nodes.insert(0, @entry)
-        @nodes.push(@exit)
+        cfg.nodes.insert(0, cfg.entry)
+        cfg.nodes.push(cfg.exit)
       end
     end
   end
