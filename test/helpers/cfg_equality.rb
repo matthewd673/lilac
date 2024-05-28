67c60d03e3ee544dca5037eb2cd6b67f5c49e564
Matthew Daly <matthewd673@outlook.com>
2024-05-27 23:10:48 -0400

    Update CFGEquality to use assertions

--- test/helpers/cfg_equality.rb
+++ test/helpers/cfg_equality.rb
@@ -2,25 +2,52 @@
 # frozen_string_literal: true
 
 require "sorbet-runtime"
+require "minitest"
 require_relative "../../lib/analysis/cfg"
 
 module CFGEquality
   extend T::Sig
+  extend Minitest::Assertions
 
   include Lilac::Analysis
 
-  sig { params(a: CFG, b: CFG).returns(T::Boolean) }
-  def self.eql?(a, b)
+  # support minitest assertions
+  class << self
+    extend T::Sig
+
+    sig { returns(Integer) }
+    attr_accessor :assertions
+
+    sig { void }
+    # This initialize is never called, but it makes Sorbet think that
+    # @assertions is defined.
+    def initialize
+      @assertions = T.let(0, Integer)
+    end
+  end
+  # This is what actually initializes @assertions to 0
+  self.assertions = 0
+
+  sig { params(actual: CFG, expected: CFG).void }
+  def self.assert_cfg_equal(actual, expected)
     # clone so IDs can be overwritten
-    a = a.clone
-    b = b.clone
+    actual = actual.clone
+    expected = expected .clone
 
     # must have same number of nodes and edges
-    if a.nodes_length != b.nodes_length || a.edges_length != b.edges_length
-      return false
-    end
+    assert_equal(actual.nodes_length,
+                 expected.nodes_length,
+                 message do
+                   "CFG node length differs: saw #{actual.nodes_length}, "\
+                   "expected #{expected.nodes_length}"
+                 end)
 
-    true
+    assert_equal(actual.edges_length,
+                 expected.edges_length,
+                 message do
+                   "CFG edges length differs: saw #{actual.edges_length}, "\
+                   "expected #{expected.edges_length}"
+                 end)
 
     # TODO: more in-depth equality check
 
