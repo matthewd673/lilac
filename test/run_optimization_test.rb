86293d8258d81e85fef77b0bfdda52100f804492
Matthew Daly <matthewd673@outlook.com>
2024-05-28 16:35:56 -0400

    Add RunOptimizationTest
    Test that all optimizations run without raising exceptions on all fancy programs (does not check correctness)

---
+++ test/run_optimization_test.rb
@@ -0,0 +1,35 @@
+# typed: strict
+# frozen_string_literal: true
+
+require "sorbet-runtime"
+require "minitest/autorun"
+require_relative "../lib/frontend/parser"
+require_relative "../lib/il"
+require_relative "../lib/analysis/cfg"
+require_relative "../lib/optimization/optimizations"
+require_relative "../lib/optimization/optimization_runner"
+
+class RunOptimizationTest < Minitest::Test
+  extend T::Sig
+
+  include Lilac
+  include Lilac::Optimization
+
+  sig { void }
+  def test_optimizations_run_without_exception
+    # attempt to perform optimizations on each "fancy" program
+    Dir["test/programs/fancy/*"].each do |f|
+      program = Frontend::Parser.parse_file(f)
+
+      OPTIMIZATIONS.each do |o|
+        # set up cfg program from scratch each time so that each optimization
+        # is running independently of each other
+        cfg_program = IL::CFGProgram.from_program(program)
+        runner = Optimization::OptimizationRunner.new(cfg_program)
+        runner.run_pass(o)
+
+        assert(true) # TODO: is there a better way to "refute_raises"?
+      end
+    end
+  end
+end
