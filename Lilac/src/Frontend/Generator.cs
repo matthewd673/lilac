800708e05bad6df623cbce74665b1de3aa539a4a
Matthew Daly <matthewd673@outlook.com>
2024-07-26 15:39:56 -0700

    Rewrite PrettyPrinter

--- Lilac/src/Frontend/Generator.cs
+++ Lilac/src/Frontend/Generator.cs
@@ -14,7 +14,7 @@ public class Generator {
     return GenerateProgram(program);
   }
 
-  private string GenerateType(IL.Type type) {
+  protected virtual string GenerateType(IL.Type type) {
     return type switch {
       IL.Type.U8 => "u8",
       IL.Type.U16 => "u16",
@@ -31,7 +31,7 @@ public class Generator {
     };
   }
 
-  private string GenerateProgram(Program program) {
+  protected virtual string GenerateProgram(Program program) {
     string str = "";
 
     foreach (GlobalDef g in program.GetGlobals()) {
@@ -49,18 +49,18 @@ public class Generator {
     return str;
   }
 
-  private string GenerateGlobalDef(GlobalDef globalDef) {
+  protected virtual string GenerateGlobalDef(GlobalDef globalDef) {
     return $"global {GenerateType(globalDef.Type)} {GenerateID(globalDef.Id)} " +
            $"= {GenerateConstant(globalDef.Rhs)}";
   }
 
-  private string GenerateExternFuncDef(ExternFuncDef externFuncDef) {
+  protected virtual string GenerateExternFuncDef(ExternFuncDef externFuncDef) {
     return $"extern func {externFuncDef.Source} {externFuncDef.Name} (" +
            $"{GenerateTypeList(externFuncDef.ParamTypes)}) -> " +
            $"{GenerateType(externFuncDef.RetType)}";
   }
 
-  private string GenerateFuncDef(FuncDef funcDef) {
+  protected virtual string GenerateFuncDef(FuncDef funcDef) {
     string str =
       $"func {funcDef.Name} ({GenerateFuncParamList(funcDef.Params)}) " +
       $"-> {GenerateType(funcDef.RetType)}\n";
@@ -74,11 +74,11 @@ public class Generator {
     return str;
   }
 
-  private string GenerateFuncParam(FuncParam funcParam) {
+  protected virtual string GenerateFuncParam(FuncParam funcParam) {
     return $"{GenerateType(funcParam.Type)} {GenerateID(funcParam.Id)}";
   }
 
-  private string GenerateStatement(Statement stmt) {
+  protected virtual string GenerateStatement(Statement stmt) {
     return stmt switch {
       Definition definition => GenerateDefinition(definition),
       Label label => GenerateLabel(label),
@@ -90,16 +90,16 @@ public class Generator {
     };
   }
 
-  private string GenerateDefinition(Definition definition) {
+  protected virtual string GenerateDefinition(Definition definition) {
     return $"{GenerateType(definition.Type)} {GenerateID(definition.Id)} = " +
            $"{GenerateExpression(definition.Rhs)}";
   }
 
-  private string GenerateLabel(Label label) {
+  protected virtual string GenerateLabel(Label label) {
     return $"{label.Name}:";
   }
 
-  private string GenerateJump(Jump jump) {
+  protected virtual string GenerateJump(Jump jump) {
     return jump switch {
       JumpZero jumpZero => $"jz {GenerateValue(jumpZero.Cond)} " +
                            $"{jumpZero.Target}",
@@ -109,20 +109,20 @@ public class Generator {
     };
   }
 
-  private string GenerateReturn(Return @return) {
+  protected virtual string GenerateReturn(Return @return) {
     return $"ret {GenerateValue(@return.Value)}";
   }
 
-  private string GenerateVoidCall(VoidCall voidCall) {
+  protected virtual string GenerateVoidCall(VoidCall voidCall) {
     return $"void {GenerateCall(voidCall.Call)}";
   }
 
-  private string GenerateInlineInstr(InlineInstr inlineInstr) {
+  protected virtual string GenerateInlineInstr(InlineInstr inlineInstr) {
     // TODO: syntax is not implemented
     throw new CannotGenerateException(inlineInstr);
   }
 
-  private string GenerateExpression(Expression expr) {
+  protected virtual string GenerateExpression(Expression expr) {
     return expr switch {
       ValueExpr valueExpr => GenerateValueExpr(valueExpr),
       BinaryOp binaryOp => GenerateBinaryOp(binaryOp),
@@ -134,17 +134,17 @@ public class Generator {
     };
   }
 
-  private string GenerateValueExpr(ValueExpr valueExpr) {
+  protected virtual string GenerateValueExpr(ValueExpr valueExpr) {
     return GenerateValue(valueExpr.Value);
   }
 
-  private string GenerateBinaryOp(BinaryOp binaryOp) {
+  protected virtual string GenerateBinaryOp(BinaryOp binaryOp) {
     return $"{GenerateValue(binaryOp.Left)} " +
            $"{GenerateBinaryOpOperator(binaryOp.Op)} " +
            $"{GenerateValue(binaryOp.Right)}";
   }
 
-  private string GenerateBinaryOpOperator(BinaryOp.Operator op) {
+  protected virtual string GenerateBinaryOpOperator(BinaryOp.Operator op) {
     return op switch {
       BinaryOp.Operator.Add => "+",
       BinaryOp.Operator.Sub => "-",
@@ -167,12 +167,12 @@ public class Generator {
     };
   }
 
-  private string GenerateUnaryOp(UnaryOp unaryOp) {
+  protected virtual string GenerateUnaryOp(UnaryOp unaryOp) {
     return $"{GenerateUnaryOpOperator(unaryOp.Op)} " +
            $"{GenerateValue(unaryOp.Value)}";
   }
 
-  private string GenerateUnaryOpOperator(UnaryOp.Operator op) {
+  protected virtual string GenerateUnaryOpOperator(UnaryOp.Operator op) {
     return op switch {
       UnaryOp.Operator.Neg => "-@",
       UnaryOp.Operator.BoolNot => "!@",
@@ -180,12 +180,12 @@ public class Generator {
     };
   }
 
-  private string GenerateConversion(Conversion conversion) {
+  protected virtual string GenerateConversion(Conversion conversion) {
     // TODO: syntax is not implemented
     throw new CannotGenerateException(conversion);
   }
 
-  private string GenerateCall(Call call) {
+  protected virtual string GenerateCall(Call call) {
     return call switch {
       ExternCall externCall => $"extern call {externCall.FuncSource} " +
                                $"{externCall.FuncName} " +
@@ -194,11 +194,11 @@ public class Generator {
     };
   }
 
-  private string GeneratePhi(Phi phi) {
+  protected virtual string GeneratePhi(Phi phi) {
     return $"phi ({GenerateValueList(new(phi.Ids))})";
   }
 
-  private string GenerateValue(Value value) {
+  protected virtual string GenerateValue(Value value) {
     return value switch {
       Constant constant => GenerateConstant(constant),
       ID id => GenerateID(id),
@@ -206,7 +206,7 @@ public class Generator {
     };
   }
 
-  private string GenerateConstant(Constant constant) {
+  protected virtual string GenerateConstant(Constant constant) {
     if (constant.Type.IsVoid()) {
       return "void";
     }
@@ -219,7 +219,7 @@ public class Generator {
     return $"{valStr}{GenerateType(constant.Type)}";
   }
 
-  private string GenerateID(ID id) {
+  protected virtual string GenerateID(ID id) {
     return id switch {
       GlobalID => $"@{id.Name}",
       ID => $"${id.Name}",
@@ -227,7 +227,7 @@ public class Generator {
     };
   }
 
-  private string GenerateTypeList(List<IL.Type> typeList) {
+  protected string GenerateTypeList(List<IL.Type> typeList) {
     string str = "";
 
     foreach (IL.Type t in typeList) {
@@ -241,7 +241,7 @@ public class Generator {
     return str;
   }
 
-  private string GenerateFuncParamList(List<FuncParam> funcParamList) {
+  protected string GenerateFuncParamList(List<FuncParam> funcParamList) {
     string str = "";
 
     foreach (FuncParam p in funcParamList) {
@@ -255,7 +255,7 @@ public class Generator {
     return str;
   }
 
-  private string GenerateValueList(List<Value> valueList) {
+  protected string GenerateValueList(List<Value> valueList) {
     string str = "";
 
     foreach (Value v in valueList) {
