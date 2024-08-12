3deab0f3ca62ad330041a6b720e0cccfd9796ea6
Matthew Daly <matthewd673@outlook.com>
2024-08-11 21:46:31 -0700

    Rewrite HexWriter

---
+++ Lilac/src/CodeGen/Targets/Wasm/HexWriter.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Text;
+
+namespace Lilac.CodeGen.Targets.Wasm;
+
+public class HexWriter : IEnumerable<byte> {
+  private List<byte> bytes = [];
+
+  public int Count => bytes.Count;
+
+  public void Write(params byte[] byteArr) {
+    bytes.AddRange(byteArr);
+  }
+
+  public void Write(List<byte> byteList) {
+    bytes.AddRange(byteList);
+  }
+
+  public void WriteAscii(string str) {
+    bytes.AddRange(Encoding.ASCII.GetBytes(str));
+  }
+
+  public void WriteUtf8(string str) {
+    bytes.AddRange(Encoding.UTF8.GetBytes(str));
+  }
+
+  public IEnumerator<byte> GetEnumerator() {
+    return bytes.GetEnumerator();
+  }
+
+  IEnumerator IEnumerable.GetEnumerator() {
+    return GetEnumerator();
+  }
+
+  public override string ToString() {
+    throw new NotImplementedException(); // TODO
+  }
+}
