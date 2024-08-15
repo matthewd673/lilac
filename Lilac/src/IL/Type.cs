ea3fda85928a5d77192452027a4ab559477e5f41
Matthew Daly <matthewd673@outlook.com>
2024-08-14 19:27:29 -0700

    Add numeric type min and max value lookup

--- Lilac/src/IL/Type.cs
+++ Lilac/src/IL/Type.cs
@@ -59,18 +59,54 @@ public static class TypeMethods {
     };
   }
 
-  public static int GetSize(this Type type) {
+  public static int GetSizeBytes(this Type type) {
     return type switch {
-      Type.U8 => 8,
-      Type.U16 => 16,
-      Type.U32 => 32,
-      Type.U64 => 64,
-      Type.I8 => 8,
-      Type.I16 => 16,
-      Type.I32 => 32,
-      Type.I64 => 64,
-      Type.F32 => 32,
-      Type.F64 => 64,
+      Type.U8 => 1,
+      Type.U16 => 2,
+      Type.U32 => 4,
+      Type.U64 => 8,
+      Type.I8 => 1,
+      Type.I16 => 2,
+      Type.I32 => 4,
+      Type.I64 => 8,
+      Type.F32 => 4,
+      Type.F64 => 8,
+      Type.Pointer => throw new NotSupportedException(),
+      Type.Void => throw new NotSupportedException(),
+      _ => throw new ArgumentOutOfRangeException(),
+    };
+  }
+
+  public static long GetMinimumValue(this Type type) {
+    return type switch {
+      Type.U8 => 0,
+      Type.U16 => 0,
+      Type.U32 => 0,
+      Type.U64 => 0,
+      Type.I8 => -128,
+      Type.I16 => -32_768,
+      Type.I32 => -2_147_483_648,
+      Type.I64 => -9_223_372_036_854_775_808,
+      Type.F32 => throw new NotImplementedException(), // TODO
+      Type.F64 => throw new NotImplementedException(), // TODO
+      Type.Pointer => throw new NotSupportedException(),
+      Type.Void => throw new NotSupportedException(),
+      _ => throw new ArgumentOutOfRangeException(),
+    };
+  }
+
+  public static ulong GetMaximumValue(this Type type) {
+    return type switch {
+      Type.U8 => 255,
+      Type.U16 => 65_535,
+      Type.U32 => 4_294_967_295,
+      Type.U64 => 18_446_744_073_709_551_615,
+      Type.I8 => 127,
+      Type.I16 => 32_767,
+      Type.I32 => 2_147_483_647,
+      Type.I64 => 9_223_372_036_854_775_807,
+      Type.F32 => throw new NotImplementedException(), // TODO
+      Type.F64 => throw new NotImplementedException(), // TODO
       Type.Pointer => throw new NotSupportedException(),
       Type.Void => throw new NotSupportedException(),
       _ => throw new ArgumentOutOfRangeException(),
